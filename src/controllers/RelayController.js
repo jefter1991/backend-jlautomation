75d56d80392bbd81be19381a8bcfd80e54a9e82e
Jefter Franciano Lopes <jefterlopes01@gmail.com>
2021-08-21 17:19:38 -0300

    Enviando a porta

--- src/controllers/RelayController.js
+++ src/controllers/RelayController.js
@@ -6,21 +6,20 @@ module.exports = {
 
    inverterRelay(request, response) {
       const {dev_ip, status, port} = request.body;
-      if(port == 1){
-        req(`http://${dev_ip}/gpio/${status}`, function (error, response, body) {
+      
+     /*   req(`http://${dev_ip}/gpio/${status}`, function (error, response, body) {
           console.log('error:', error); // Print the error if one occurred
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-          console.log('body:', body); // Print the HTML for the Google homepage.
-        });
-        return response.json(response.statusCode);
-      }else{
+          console.log('body:', body); // Print the HTML for the Google homepage.*/   
+      
         //enviando a porta
-        req(`http://${dev_ip}/gpio/${port}/${status}`, function (error, response, body) {
+        req(`http://${dev_ip}/gpio/${status}/${port}`, function (error, response, body) {
           console.log('error:', error); // Print the error if one occurred
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
           console.log('body:', body); // Print the HTML for the Google homepage.
         });
-      }
+        
+        return response.json(response.statusCode);
           
   } 
 
